58d04a4d0750f5ca5b1e3e8c52cd163a33c0fdfa
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-11-24 21:01:21 +0700

    Release v0.001

--- lib/App/TextWrapUtils.pm
+++ lib/App/TextWrapUtils.pm
@@ -18,7 +18,7 @@ our @BACKENDS = qw(
                       Text::ANSI::WideUtil
                       Text::Fold
                       Text::LineFold
-                      Text::Wide::Util
+                      Text::WideChar::Util
                       Text::Wrap
               );
 
@@ -34,7 +34,6 @@ _
         filename => {
             schema => 'filename*',
             default => '-',
-            req => 1,
             pos => 0,
             description => <<'_',
 
@@ -50,6 +49,7 @@ _
         # XXX arg: initial indent string/number of spaces?
         # XXX arg: subsequent indent string/number of spaces?
         # XXX arg: option to not wrap verbatim paragraphs
+        # XXX arg: pass per-backend options
         backend => {
             schema => ['perl::modname*', in=>\@BACKENDS],
             default => 'Text::Wrap',
@@ -68,18 +68,32 @@ sub textwrap {
 
     log_trace "Using text wrapping backend %s", $backend;
 
-    my @paras = split /\R{2,}/, $text;
+    my @paras = split /(\R{2,})/, $text;
 
     my $res = '';
     while (my ($para_text, $blank_lines) = splice @paras, 0, 2) {
+        $para_text =~ s/\R/ /g;
+
         if ($backend eq 'Text::ANSI::Fold') {
             require Text::ANSI::Fold;
             $para_text = join("", map {"$_\n"} Text::ANSI::Fold->new(width=>$width)->text($para_text)->chops);
+            $para_text =~ s/\R\z//;
         } elsif ($backend eq 'Text::ANSI::Util') {
+            require Text::ANSI::Util;
+            $para_text = Text::ANSI::Util::ta_wrap($para_text, $width);
         } elsif ($backend eq 'Text::ANSI::WideUtil') {
+            require Text::ANSI::WideUtil;
+            $para_text = Text::ANSI::WideUtil::ta_mbwrap($para_text, $width);
         } elsif ($backend eq 'Text::Fold') {
+            require Text::Fold;
+            $para_text = Text::Fold::fold_text($para_text, $width);
         } elsif ($backend eq 'Text::LineFold') {
-        } elsif ($backend eq 'Text::Wide::Util') {
+            require Text::LineFold;
+            $para_text = Text::LineFold->new(ColMax => $width)->fold('', '', $para_text);
+            $para_text =~ s/\R\z//;
+        } elsif ($backend eq 'Text::WideChar::Util') {
+            require Text::WideChar::Util;
+            $para_text = Text::WideChar::Util::mbwrap($para_text, $width);
         } elsif ($backend eq 'Text::Wrap') {
             require Text::Wrap;
             local $Text::Wrap::columns = $width;
@@ -88,7 +102,7 @@ sub textwrap {
             return [400, "Unknown backend '$backend'"];
         }
 
-        $res .= $para_text . $blank_lines;
+        $res .= $para_text . ($blank_lines // "");
     }
     [200, "OK", $res];
 }
@@ -101,6 +115,8 @@ sub textwrap {
 This distributions provides the following command-line utilities:
 
 # INSERT_EXECS_LIST
+
+Keywords: fold.
 
 
 =head1 SEE ALSO
